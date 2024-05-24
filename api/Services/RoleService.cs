f4095ad29fa916817a609cfa8de7c79079881074
Atom <phakawat.b.soft@gmail.com>
2024-05-24 17:45:23 +0700

    Atom Practice

--- api/Services/RoleService.cs
+++ api/Services/RoleService.cs
@@ -1,4 +1,6 @@
 ﻿using api.Models;
+using System.Data;
+using System.Xml.Linq;
 
 namespace api.Services;
 
@@ -9,4 +11,38 @@ public class RoleService
     {
         _dbContext = dbContext;
     }
+    public List<Role> GetRoles()
+    {
+        return _dbContext.Roles.OrderBy(role => role.Name).ToList();
+    }public List<Role> GetRoleByName(string name)
+    {
+        return _dbContext.Roles.Where(role => role.Name == name).ToList();
+    }
+    public Role AddRole(Role role) {
+        _dbContext.Roles.Add(role);
+        _dbContext.SaveChangesAsync();
+        return role;
+    }
+
+    public Role DeleteRole(Guid id)
+    {
+        Role role = _dbContext.Roles.Where(role => role.Id == id).FirstOrDefault();
+        _dbContext.Roles.Remove(role);
+        _dbContext.SaveChangesAsync();
+        return role;
+    }
+
+    public Role UpdateRole(Role role)
+    {
+        Role roleToUpdate = _dbContext.Roles.Where(_role => _role.Id.ToString() == role.Name).FirstOrDefault();
+        role.Name = "test";
+        role.Active = true;
+        role.Description = "test";
+        role.CreatedDate = DateTime.Now;
+        role.CreatedBy = "Program";
+        _dbContext.Roles.Attach(role);
+        _dbContext.SaveChangesAsync();
+        return role;
+    }
+
 }
--- api/Services/UserService.cs
+++ api/Services/UserService.cs
@@ -1,4 +1,6 @@
 ﻿using api.Models;
+using Microsoft.VisualBasic;
+using System.Collections;
 
 namespace api.Services;
 
@@ -8,5 +10,52 @@ public class UserService
     public UserService(dbContext dbContext)
     {
         _dbContext = dbContext;
+    }
+
+    public async Task<List<User>> GetUser()
+    {
+        List<User> user = (List<User>)await _dbContext.QueryAsync<User>(@"select id as ""Id"", first_name as ""FirstName"" , last_name as ""LastName"" , gender as ""Gender"" from public.user");
+        return user;
+    }
+
+    public async Task<List<User>> GetUserByName(string name)
+    {
+        string sql = @"select id as ""Id"", first_name as ""FirstName"" , last_name as ""LastName"" , gender as ""Gender"" from public.user where first_name = @firstname or last_name = @lastname";
+        List<User> _user = (List<User>)await _dbContext.QueryAsync<User>(sql, new
+        {
+            firstname = name,
+            lastname = name,
+        });
+        return _user;
+    }
+
+    public async Task<User> AddUser(User user)
+    {
+        string sql = "insert into public.user (first_name,last_name,gender) values (@firstname,@lastname,@gender)";
+        User _user = await _dbContext.QueryFirstOrDefaultAsync<User>(sql, new
+        {
+            firstname = user.FirstName.ToString(),
+            lastname = user.LastName.ToString(),
+            gender = user.Gender,
+        });
+        return _user;
+    }
+
+    public async Task<User> UpdateUser(User user)
+    {
+        string sql = @"update public.user set first_name = @firstname,last_name = @lastname,gender = @gender where id = @id";
+        User _user = await _dbContext.QueryFirstOrDefaultAsync<User>(sql, new {
+            id = user.Id,
+            firstname = user.FirstName.ToString(),
+            lastname = user.LastName.ToString(),
+            gender = user.Gender,
+        });
+
+        return _user;
+    }
+
+    public async Task<User> DeleteUser(User user)
+    {
+        return await _dbContext.QueryFirstOrDefaultAsync<User>(@"delete from public.user where id = @id",new {id = user.Id});
     }
 }
