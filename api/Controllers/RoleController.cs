62af5e66becc2123d2c30733162807c79e51d7da
NuyNuy <baisaenc@gmail.com>
2024-06-21 12:59:44 +0700

    NuyNuy Practice

--- api/Controllers/RoleController.cs
+++ api/Controllers/RoleController.cs
@@ -1,4 +1,8 @@
-﻿using api.Services;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+using api.Models;
+using api.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers;
@@ -11,6 +15,38 @@ public class RoleController : ControllerBase
 
     public RoleController(RoleService roleService)
     {
-        _roleService = roleService;
+        this._roleService = roleService;
     }
+
+    [HttpGet("All")]
+    public async Task<ActionResult> Get()
+    {
+        return Ok(this._roleService.GetRolesAll());
+    }
+
+    [HttpGet("Serch")]
+    public async Task<ActionResult> Get([FromQuery] string name)
+    {
+        return Ok(this._roleService.SerchRole(name));
+    }
+
+    [HttpPost("Add")]
+    public async Task<ActionResult> Post([FromBody] Role role)
+    {
+        string status = await this._roleService.AddRole(role);
+        return Ok(status);
+    }
+
+    [HttpPut("Update")]
+    public async Task<ActionResult> Update([FromBody] Role role)
+    {
+        return Ok(this._roleService.UpdateRole(role));
+    }
+
+    [HttpDelete("Delete")]
+    public async Task<ActionResult> Delete([FromBody] Guid Id) 
+    {
+        return Ok(this._roleService.DeleteRole(Id));
+    }
+
 }
--- api/Services/UserService.cs
+++ api/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using api.Models;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using api.Models;
 
 namespace api.Services;
 
@@ -9,4 +11,73 @@ public class UserService
     {
         _dbContext = dbContext;
     }
+
+    public async Task<List<User>> GetUser()
+    {
+        return (List<User>)await _dbContext.QueryAsync<User>("SELECT id, first_name AS firstname, last_name AS lastname, gender FROM public.user ORDER BY UPPER(first_name), UPPER(last_name)");
+    }
+
+    public async Task<User> SearchUser(string FirstName, string LastName)
+    {
+        return (User) await this._dbContext.QueryFirstOrDefaultAsync<User>("SELECT id, first_name AS firstname, last_name AS lastname, gender FROM public.user WHERE UPPER(first_name) = UPPER(@fname) or UPPER(last_name) = UPPER(@lname)", new { fname = FirstName, lname = LastName });
+    }
+
+    public async Task<string> AddUser(User user)
+    {
+        string name = (string)await this._dbContext.ExecuteScalarAsync<string>("SELECT first_name AS name FROM public.user WHERE UPPER(first_name) = UPPER(@fname) AND UPPER(last_name) = UPPER(@lname)", new { fname = user.FirstName, lname = user.LastName });
+        if (name == null)
+        {
+            if ((await this._dbContext.ExecuteAsync(@"INSERT INTO public.user(id, first_name, last_name, gender) VALUES(@id, @fname, @lname, @gender)", new { id = Guid.NewGuid(), fname = user.FirstName, lname = user.LastName, gender = user.Gender })) != 0)
+            {
+                return "Add success";
+            }
+            else
+            {
+                return "Add fail";
+            }
+        }
+        else
+        {
+            return "Add fail duplicate";
+        }
+    }
+
+    public async Task<string> UpdateUser(User user)
+    {
+        User isDuplicateName = (User)await this._dbContext.QueryFirstOrDefaultAsync<User>("SELECT id FROM public.user WHERE UPPER(first_name) = UPPER(@fname) and UPPER(last_name) = UPPER(@lname) and id <> @id", new { fname = user.FirstName, lname = user.LastName, id = user.Id });
+        if (isDuplicateName == null)
+        {
+            User selectUpdate = (User)await this._dbContext.QueryFirstOrDefaultAsync<User>("SELECT id, first_name AS firstname, last_name AS lastname, gender FROM public.user WHERE id = @id", new { id = user.Id });
+            if (selectUpdate != null)
+            {
+                int executed = await this._dbContext.ExecuteAsync(@"UPDATE public.user SET first_name = @fname, last_name = @lname, gender = @gender WHERE id = @id", new {id = user.Id, fname = user.FirstName, lname = user.LastName, gender = user.Gender });
+                if (executed != 0)
+                {
+                    return ("Update success");
+                }
+                else
+                {
+                    return ("Update fail");
+                }
+            }
+            else
+            {
+                return ("Update fail not found");
+            }
+            
+        }
+        else
+        {
+            return ("Update fail duplicate");
+        }
+    }
+
+    public async Task<string> DeleteUser(Guid id)
+    {
+        User user = (User)await this._dbContext.QueryFirstOrDefaultAsync<User>("SELECT id, first_name AS firstname, last_name AS lastname, gender FROM public.user WHERE id = @id", new { id = id });
+        await this._dbContext.ExecuteAsync(@"DELETE FROM public.user WHERE id = @id", new { id = user.Id });
+        return "Success";
+    }
+
+
 }
